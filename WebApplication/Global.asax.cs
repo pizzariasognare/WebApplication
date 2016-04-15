306ec179ea966168cc27330feed7ee487155253c
pizzariasognare <pizzariasognare@gmail.com>
2016-04-14 23:45:34 -0300

    Corrigido a lentidão no site.

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -13,6 +13,8 @@ namespace WebApplication
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine()); 
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
